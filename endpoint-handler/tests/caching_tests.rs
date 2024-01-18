1e5f9e4f3120bd748b8ff222686057cf45f17cf8
David Yen <david99900@gmail.com>
2024-01-18 20:32:42 +0800

    complete endpoint caching

--- endpoint-handler/tests/caching_tests.rs
+++ endpoint-handler/tests/caching_tests.rs
@@ -1,11 +1,10 @@
 #[cfg(test)]
 mod caching_tests {
-    use std::ops::Deref;
-
     use endpoint_handler::{
         caching::{TemplateCaching, TemplatesCachingImpl},
         endpoint_template::TemplateImpl,
     };
+    use std::ops::Deref;
 
     #[test]
     pub fn create_new_cache() {
@@ -22,13 +21,20 @@ mod caching_tests {
         let mock_template_2 = TemplateImpl::new("/test".to_string(), "POST".to_string(), None);
         let mock_template_3 = TemplateImpl::new("/test".to_string(), "PUT".to_string(), None);
 
+        let mock_template_other = TemplateImpl::new("/other".to_string(), "GET".to_string(), None);
+        let mock_template_other_2 =
+            TemplateImpl::new("/other".to_string(), "POST".to_string(), None);
+
         template_cache.add_range(
             "test",
             vec![mock_template_1, mock_template_2, mock_template_3],
         );
+        template_cache.add_range("other", vec![mock_template_other, mock_template_other_2]);
 
         let templates = template_cache.get("test").unwrap();
+        let other_templates = template_cache.get("other").unwrap();
 
         assert_eq!(templates.len(), 3);
+        assert_eq!(other_templates.len(), 2);
     }
 }
