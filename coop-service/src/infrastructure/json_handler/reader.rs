4c03221320fc096875c4722dbbd481beda5c679e
David Yen <david99900@gmail.com>
2024-01-18 23:21:05 +0800

    add json reader

--- coop-service/src/infrastructure/json_handler/reader.rs
+++ coop-service/src/infrastructure/json_handler/reader.rs
@@ -1,32 +1,14 @@
-pub struct JsonReaderImpl {
-    file_path: String,
-}
+use serde::de::DeserializeOwned;
+use tokio::{fs::File, io::BufReader};
 
-pub trait JsonReader<T> {
-    fn new(file_path: String) -> Self;
-    fn read(&self) -> T;
-}
-
-impl JsonReader for JsonReaderImpl {
-    fn new(file_path: Option<String>) -> Self {
-        let path = match file_path {
-            Some(path) => path,
-            None => String::from(""),
-        };
-
-        Self { file_path: path }
-    }
-
-    fn read(&self) -> String {
-        let file = File::open(&self.file_path).unwrap();
-        let reader = BufReader::new(file);
-
-        let mut json = String::new();
-
-        for line in reader.lines() {
-            json.push_str(&line.unwrap());
-        }
-
-        json
-    }
+pub async fn read_json_file<T>(path: &str) -> Result<T, String>
+where
+    T: DeserializeOwned,
+{
+    let file = match File::open(path).await {
+        Ok(file) => file,
+        Err(err) => return Err(err.to_string()),
+    };
+    let reader = BufReader::new(file);
+    todo!()
 }
