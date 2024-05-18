75206aa4c29b0d9e7b90ac109c679fc94fd47720
David Yen <david99900@gmail.com>
2024-05-18 13:33:23 +0800

    add limitation for some varchar columns on Endpoints table

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -2,6 +2,7 @@ pub use sea_orm_migration::prelude::*;
 
 mod m20220101_000001_create_table;
 mod m20240106_032056_add_options_col_to_endpoints;
+mod m20240515_140830_adjust_endpoint_table_datatypes;
 
 pub struct Migrator;
 
@@ -11,6 +12,7 @@ impl MigratorTrait for Migrator {
         vec![
             Box::new(m20220101_000001_create_table::Migration),
             Box::new(m20240106_032056_add_options_col_to_endpoints::Migration),
+            Box::new(m20240515_140830_adjust_endpoint_table_datatypes::Migration),
         ]
     }
 }
